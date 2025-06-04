e3a157f41f3eeb0e90d294c42cb41a6aa742b03b
Adaobilynda1234 <adaobilynda1234@gmail.com>
2025-06-04 11:23:15 +0100

    updated hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import Experience from "./components/Experience";
+import Hero from "./components/Hero";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Skills from "./components/Skills";
 import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
 
 export default function App() {
   return (
     <>
       <Navbar />
-      {/* <Hero /> */}
+      <Hero />
       <Skills />
       <Portfolio />
       <Experience />
-      {/* <Contact /> */}
+      <Contact />
       <Footer />
     </>
   );
